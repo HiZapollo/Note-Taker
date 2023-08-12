018c235208783043b2fc1d019b979734eeab05a8
zeckenrode <zeckenrode9000@gmail.com>
2023-08-12 09:36:19 -0400

    added the api calls

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +13,54 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('/db/db.json', (err, data) => {
+        if(err){
+            console.log(err);
+        } else {
+            const parsedReviews = JSON.parse(data);
+            res.json(parsedReviews);
+        }
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if(title && text){
+        const newNote = {
+            title,
+            text,
+        };
+
+        //const noteString = JSON.stringify(newNote);
+
+        fs.readFile('/db/db.json', (err, data) => {
+            if(err){
+                console.log(err);
+            } else {
+                const parsedNote = JSON.parse(data);
+                parsedNote.push(newNote);
+                fs.writeFile(`/db/db.json`, JSON.stringify(parsedNote), (err) => {
+                    err
+                        ? console.log(err)
+                        : console.log(`Note '${newNote.title}' has been written successfully`)
+                });
+            }
+        })
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error posting note');
+    }
 });
 
 app.listen(PORT, () => {
